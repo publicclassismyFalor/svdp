78ab5ee5504270956723c4c2090004abaf9096aa
kt <kt@kissos.org>
2018-02-11 17:35:22 +0800

    ts

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ mod memcache;
 mod mongodb;
 
 use std::thread;
+use std::time::Duration;
 
 pub const CMD: &str = "/tmp/aliyun_cmdb";
 pub const ARGV: &[&str] = &["-userId", "LTAIHYRtkSXC1uTl", "-userKey", "l1eLkvNkVRoPZwV9jwRpmq1xPOefGV"];
@@ -16,20 +17,33 @@ pub static mut BASESTAMP: u64 = 0;
 pub const INTERVAL: u64 = 5 * 60 * 1000;
 
 pub fn run() {
-    unsafe {
-        BASESTAMP = 1000 * (std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs() - 5 * 60);
-    }
+    let ts_now = || -> u64 {1000 * std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs()};
 
-    let mut tids = vec![];
+    unsafe { BASESTAMP = ts_now() - INTERVAL; }
 
-    tids.push(thread::spawn(|| ecs::sv()));
-    tids.push(thread::spawn(|| slb::sv()));
-    tids.push(thread::spawn(|| rds::sv()));
-    tids.push(thread::spawn(|| redis::sv()));
-    tids.push(thread::spawn(|| memcache::sv()));
-    tids.push(thread::spawn(|| mongodb::sv()));
+    let mut basestamp;
+    loop {
+        unsafe { basestamp = BASESTAMP; }
 
-    for tid in tids.into_iter() {
-        tid.join().unwrap();
+        while ts_now() > (basestamp + INTERVAL) {
+            let mut tids = vec![];
+
+            tids.push(thread::spawn(|| ecs::sv()));
+            tids.push(thread::spawn(|| slb::sv()));
+            tids.push(thread::spawn(|| rds::sv()));
+            tids.push(thread::spawn(|| redis::sv()));
+            tids.push(thread::spawn(|| memcache::sv()));
+            tids.push(thread::spawn(|| mongodb::sv()));
+
+            for tid in tids.into_iter() {
+                tid.join().unwrap();
+            }
+
+            basestamp += INTERVAL;
+        }
+
+        unsafe { BASESTAMP = basestamp; }
+
+        thread::sleep(Duration::from_secs(INTERVAL));
     }
 }
