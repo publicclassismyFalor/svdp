9336b2ce9675dde765cc192fa512db201552e198
test <test@test>
2018-02-23 15:52:41 +0800

    TODO: query sql cmd

--- src/lib.rs
+++ src/lib.rs
@@ -14,8 +14,8 @@ mod dp;
 
 use std::thread;
 use std::fs::File;
-use std::io::Read;
-use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+use std::net::TcpListener;
 
 use threadpool::ThreadPool;
 use r2d2_postgres::{TlsMode, PostgresConnectionManager};
@@ -44,6 +44,26 @@ fn conf_parse() -> Config {
 }
 
 /* json rpc service on tcp */
+#[derive(Serialize, Deserialize)]
+struct Req {
+    method: String,
+    params: Params,
+    id: i32,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Params {
+    instance_id: Option<Vec<String>>,
+    ts_range: [i32; 2],
+}
+
+/// REQ example:
+/// {"method":"sv_ecs","params":{"instance_id":"i-123456","ts_range":[15000000,1600000]},"id":0}
+///
+/// RES example:
+/// {"result":"{...}","id":0}
+/// OR
+/// {"err":"...","id":0}
 fn jsonrpc_serv() {
     let tdpool = ThreadPool::new(20);
 
@@ -59,13 +79,50 @@ fn jsonrpc_serv() {
 
     loop {
         match listener.accept() {
-            Ok((_socket, _addr)) => {
+            Ok((mut socket, _peeraddr)) => {
                 let pgpool = pgpool.clone();
-
                 tdpool.execute(move|| {
-                    let pgconn = pgpool.get().unwrap_or_else(|e| { errexit!(e); });
+                    let mut buf = String::new();
+                    while 0 < socket.read_to_string(&mut buf)
+                        .unwrap_or_else(|e|{ errexit!(e); }) {}
+
+                    let req: Req = serde_json::from_str(&buf).unwrap_or_else(|e| {
+                        socket.write("{\"err\":\"json parse err\",\"id\":-1}".as_bytes())
+                            .unwrap_or_else(|err|{ errexit!(err); });
+                        errexit!(e);
+                    });
+
+                    let querysql = String::new();
+                    match req.params.instance_id {
+                        None => {
+                        },
+                        _ => {
+                        }
+                    }
+
+                    let pgconn = pgpool.get().unwrap_or_else(|e| {
+                        socket.write(format!("{}\"err\":\"db_conn_pool busy\",\"id\":{}{}", "{", req.id, "}").as_bytes())
+                            .unwrap_or_else(|err|{ errexit!(err); });
+                        errexit!(e);
+                    });
+
+                    let qres = pgconn.query(querysql.as_str(), &[]).unwrap_or_else(|e| {
+                        socket.write(format!("{}\"err\":\"db query err\",\"id\":{}{}", "{", req.id, "}").as_bytes())
+                            .unwrap_or_else(|err|{ errexit!(err); });
+                        errexit!(e);
+                    });
+
+                    let resrow = qres.get(0);
+                    let res = resrow.get_bytes(0).unwrap_or_else(|| {
+                        socket.write(format!("{}\"err\":\"empty result\",\"id\":{}{}", "{", req.id, "}").as_bytes())
+                            .unwrap_or_else(|err|{ errexit!(err); });
+                        errexit!("empty result");
+                    });
+
+                    socket.write(res).unwrap_or_else(|e|{ errexit!(e); });
                 });
             },
+
             Err(e) => err!(e)
         }
     }
