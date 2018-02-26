9dfed4412e4067fcd9ca8cc17bc1bcf24ea20afc
test <test@test>
2018-02-26 15:00:23 +0800

    处理值为 null 时的情况

--- src/lib.rs
+++ src/lib.rs
@@ -227,12 +227,12 @@ fn worker(body: &Vec<u8>) -> Result<(String, i32), String> {
     if let Some(orig) = qres.get(0) {
         let orig: String = orig;
         let mut finalres = vec![vec![], vec![]];
-        if let Ok(mut r) = serde_json::from_str::<Vec<[i32; 2]>>(&orig) {
-            r.sort_by(|a, b|a[0].cmp(&b[0]));
+        if let Ok(mut r) = serde_json::from_str::<Vec<(i32, Option<i32>)>>(&orig) {
+            r.sort_by(|a, b|a.0.cmp(&b.0));
             let len = r.len();
             for i in 0..len {
-                finalres[0].push(r[i][0]);
-                finalres[1].push(r[i][1]);
+                finalres[0].push(r[i].0);
+                finalres[1].push(if None == r[i].1 {0} else {r[i].1.unwrap()});
             }
         } else {
             err!("server err");
